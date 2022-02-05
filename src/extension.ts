25f0a06ff01be7df15d00d5ff1b2b3407d66fc5b
Stephan Renggli <5135902+stephanrenggli@users.noreply.github.com>
2022-02-05 18:22:47 +0100

    Add highlight color settings (#27)
    * Add highlight color settings

--- src/extension.ts
+++ src/extension.ts
@@ -14,6 +14,7 @@ export function activate(context: vscode.ExtensionContext) {
     console.log('settings changed');
     if (workspaceConfig.get<Array<boolean>>('extension.activated')) {
       triggerUpdate();
+      Decorator.init();
     }
   });
 
